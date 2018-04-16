4754b57b49283b991ef1a63dea4d5ab1adabd4e9
Christoph Welcz <christoph@welcz.de>
2018-04-16 14:52:11 +0200

    added some explaining to the second shell script

--- inject-tweaks.sh
+++ inject-tweaks.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-## Current directory
-## http://stackoverflow.com/a/246128
+# inject tweaks without downloading and extracting reveal js.
+# useful for debugging!
 CURRENT_DIR=$PWD
-
-## inject tweaks
 cp -rf ${CURRENT_DIR}/tweaks/* ${CURRENT_DIR}/slides/
